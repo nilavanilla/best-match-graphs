12f1e4ddaf6ef49b2ffb8583ccb21f463c5c553e
nilavanilla <65246360+nilavanilla@users.noreply.github.com>
2020-05-13 12:23:52 +0200

    Update cdigraph.h

--- cdigraph.h
+++ cdigraph.h
@@ -20,20 +20,20 @@ class CDigraph	{
 		Tree lrt;
 		
 		CDigraph()	{};
-		CDigraph(std::vector<Vertex> ver, std::set<int> spec);		//line 5
-		CDigraph(std::vector<int> ver, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert);	//line 12
+		CDigraph(std::vector<Vertex> ver, std::set<int> spec);		//line 9
+		CDigraph(std::vector<int> ver, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert);	//line 16
 
-		int pos_of_Vert(int nr);		//line 35
+		int pos_of_Vert(int nr);		//line 39
 		bool find_conComp(std::vector<int> indVertSet, std::vector<std::vector<int> > &indCompSet, std::set<int> indSpec, bool e);		//line 47
-		void induced_vertexSet(int s, int t, std::vector<int> &indVertSet, int c);		//line 112
+		void induced_vertexSet(int s, int t, std::vector<int> &indVertSet, int c);		//line 116
 
-		void find_classes(std::vector<int> vert);	//line 123
-		bool check_N1();		//line 173
-		bool check_N2();		//line 212
-		bool check_N3();		//line 224
+		void find_classes(std::vector<int> vert);	//line 127
+		bool check_N1();		//line 177
+		bool check_N2();		//line 216
+		bool check_N3();		//line 228
 
-		void find_infTriples(std::vector<int> v);		//line 248
+		void find_infTriples(std::vector<int> v);		//line 252
 		void build_GofT(std::vector<int> subgraphs, std::vector<std::set<int> > leafs, Tree t0);
-		bool check_GofT(std::set<int> v, bool e);		//line 396
+		bool check_GofT(std::set<int> v, bool e);		//line 400
 };
 #endif //_CDIGRAPH_H_
