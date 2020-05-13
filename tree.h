27361c2ff1378828abd91a36ff24eb4ceb51f726
nilavanilla <65246360+nilavanilla@users.noreply.github.com>
2020-05-13 12:27:07 +0200

    Update tree.h

--- tree.h
+++ tree.h
@@ -18,21 +18,21 @@ class Tree	{
 		std::vector<int> classes;					//the entries contain the class number if the represented node is a leaf, in the other case there is a -1 at this position (is only needed if algorithm 1 is chosen)
 		std::vector<std::vector<int> > edgesGofT;	//adjacency matrix of the cBMG G(T,sigma)
 
-		Tree();		//line 6
+		Tree();		//line 10
 
-		void connect_trees(Tree subtree);		//line 16
+		void connect_trees(Tree subtree);		//line 20
 
-		void build_hasse(std::vector<std::vector<int> > p, std::vector<Class> classes, int a, bool iN);	//line 45
-		int compute_par(std::vector<int> &x, std::vector<std::vector<int> > p);	//line 121
-		bool check_tree(std::vector<std::set<int> > r, std::vector<Vertex> v);	//line 143
+		void build_hasse(std::vector<std::vector<int> > p, std::vector<Class> classes, int a, bool iN);	//line 49
+		int compute_par(std::vector<int> &x, std::vector<std::vector<int> > p);	//line 125
+		bool check_tree(std::vector<std::set<int> > r, std::vector<Vertex> v);	//line 147
 
-		void find_leafSets(int n, std::vector<int> &subgraphs, std::vector<std::set<int> > &leafs, bool v);	//line 223
-		int find_leafs(std::vector<int> subgraph, int n);	//line 249
-		int pos_of_Vert(int n, std::vector<Vertex> v);	//lien 261
+		void find_leafSets(int n, std::vector<int> &subgraphs, std::vector<std::set<int> > &leafs, bool v);	//line 227
+		int find_leafs(std::vector<int> subgraph, int n);	//line 253
+		int pos_of_Vert(int n, std::vector<Vertex> v);	//lien 265
 
-		bool build_aho(std::set<int> leafs, std::vector<std::vector<int> > triples, int p, std::vector<Vertex> vert, bool e);	//line 273
+		bool build_aho(std::set<int> leafs, std::vector<std::vector<int> > triples, int p, std::vector<Vertex> vert, bool e);	//line 277
 
-		void matrix_to_vector(std::vector<std::vector<int> > adj, std::vector<int> spec, std::vector<int> nr, std::vector<std::string> names, std::vector<Vertex> &vert, int nrIN);	//line 349
+		void matrix_to_vector(std::vector<std::vector<int> > adj, std::vector<int> spec, std::vector<int> nr, std::vector<std::string> names, std::vector<Vertex> &vert, int nrIN);	//line 353
 };
 
 #endif //_TREE_H_
