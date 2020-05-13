25e69d125d7670230bb905862ee2d5079e76990f
nilavanilla <65246360+nilavanilla@users.noreply.github.com>
2020-05-13 12:25:39 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,16 +8,16 @@
 #include "cdigraph.h"
 
 //defined in dataCollection.cc
-long collect_data(int nr, int nrL, int nrS, int nrDel, int nrR);	//line 5
-void print_progress(const float& percentage);	//line 214
+long collect_data(int nr, int nrL, int nrS, int nrDel, int nrR);	//line 9
+void print_progress(const float& percentage);	//line 218
 
 //defined in drawing.cc
 void draw_graph(CDigraph graph);
 
 //defined in buildLRT.cc
-bool build_lrTree(CDigraph &graph, int alg, std::vector<int> &fails);	//line 5
-bool alg_1(std::vector<int> vert, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert, std::vector<std::vector<int> > &infTriples);	//line 99
-bool alg_2(std::vector<int> vert, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert, std::vector<std::vector<int> > &infTriples, std::vector<int> &fails);	//line 162
-void find_triples(std::vector<std::vector<int> > indComp, std::vector<std::vector<int> > &indTrip);	//line 204
+bool build_lrTree(CDigraph &graph, int alg, std::vector<int> &fails);	//line 9
+bool alg_1(std::vector<int> vert, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert, std::vector<std::vector<int> > &infTriples);	//line 103
+bool alg_2(std::vector<int> vert, std::vector<std::vector<int> > adj, std::vector<Vertex> allVert, std::vector<std::vector<int> > &infTriples, std::vector<int> &fails);	//line 166
+void find_triples(std::vector<std::vector<int> > indComp, std::vector<std::vector<int> > &indTrip);	//line 208
 
 #endif //_FUNCTIONS_H_
